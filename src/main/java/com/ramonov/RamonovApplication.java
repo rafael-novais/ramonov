6d6baceeec85a2388237bef7d35d49822138bc43
rafao <rafael.novais.amorim@gmail.com>
2025-11-03 21:52:44 -0300

    welcome ramonov

---
+++ src/main/java/com/ramonov/RamonovApplication.java
@@ -0,0 +1,13 @@
+package com.ramonov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamonovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamonovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonov/RamonovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamonovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
